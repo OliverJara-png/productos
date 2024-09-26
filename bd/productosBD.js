041ae94897a8df1c672fc60a6d176221d6e510e8
OliverJara-png <giovannijd.ti23@utsjr.edu.mx>
2024-09-25 23:33:50 -0600

    first commit

---
+++ bd/productosBD.js
@@ -0,0 +1,70 @@
+const productosBD = require("./conexion").productos;
+const Producto = require("../clases/ProductoClase");
+
+function validarDatosProducto(producto) {
+    var datosCorrectos = false;
+    
+    if (producto.producto != undefined && producto.cantidad != undefined && producto.precio != undefined) {
+        datosCorrectos = true;
+    }
+    
+    return datosCorrectos;
+}
+
+async function mostrarProductos() {
+    const productos = await productosBD.get();
+    var productosValidos = [];
+    
+    productos.forEach(producto => {
+        const productoData = new Producto({id: producto.id, ...producto.data()});
+        const productoValido = productoData.getproducto;
+        if (validarDatosProducto(productoValido)) {
+            productosValidos.push(productoValido);
+        }
+    });
+    
+    return productosValidos;
+}
+
+async function buscarProductoPorId(id) {
+    const producto = await productosBD.doc(id).get();
+    const productoData = new Producto({id: producto.id, ...producto.data()});
+    var productoValido = { error: true };
+    
+    if (validarDatosProducto(productoData.getproducto)) {
+        productoValido = productoData.getproducto;
+    }
+    
+    return productoValido;
+}
+
+async function nuevoProducto(data) {
+    const productoData = new Producto(data);
+    var productoValido = false;
+    
+    if (validarDatosProducto(productoData.getproducto)) {
+        await productosBD.doc().set(productoData.getproducto);
+        productoValido = true;
+    }
+    
+    return productoValido;
+}
+
+async function borrarProducto(id) {
+    const producto = await buscarProductoPorId(id);
+    var borrado = false;
+    
+    if (producto.error != true) {
+        await productosBD.doc(id).delete();
+        borrado = true;
+    }
+    
+    return borrado;
+}
+
+module.exports = {
+    mostrarProductos,
+    nuevoProducto,
+    borrarProducto,
+    buscarProductoPorId
+};
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const usuariosRutas = require("./rutas/rutasUsuarios");
+const productosRutas = require("./rutas/rutasProductos");
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", usuariosRutas);
+app.use("/productos", productosRutas);
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log("Servidor en http://localhost:" + port);
+});
